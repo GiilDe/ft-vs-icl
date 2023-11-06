f7a27bdcbc4be8cbef2ac3cc4462bc13dff1aa25
Guy Dar <dar@mail.tau.ac.il>
2023-11-07 00:05:42 +0200

    beautify run.sh

--- run.sh
+++ run.sh
@@ -33,11 +33,11 @@ while [[ $# -gt 0 ]]; do
             shift 2
             ;;
         --lr)
-            lr="$2"
+            lr=$2
             shift 2
             ;;
         --clip_norm)
-            clip_norm="$2"
+            clip_norm=${2}
             shift 2
             ;;
         --per_layer)
@@ -45,7 +45,7 @@ while [[ $# -gt 0 ]]; do
             shift 2
             ;;
         *)
-            echo "Unknown option: $1"
+            echo "!!!!! Unknown option: $1 !!!!!"
             exit 1
             ;;
     esac
@@ -70,27 +70,19 @@ bpe_path=$base_dir/gpt_icl/vocab.bpe
 encoder_path=$base_dir/gpt_icl/encoder.json
 dict_path=$base_dir/gpt_icl/$model_name/dict.txt
 output_path=$output_base_dir/${SLURM_JOBID}
-
 ana_rlt_dir=$base_dir/ana_rlt/$model_name/${task}_${SLURM_JOBID}
-
-# TODO: solve the problem of the following two lines
-# rm -r tmp_ana_rlt
-# mkdir -p tmp_ana_rlt
-
-# ==================== train the model for analyzing FT setting ============
-
-k=$icl_k
 ana_attn=1
+
+# ============= Train FT Model =============
+echo "Train FT Model"
+k=$icl_k
 ana_setting=ft
 model_path=$base_dir/gpt_icl/$model_name/model.pt
 rm $ana_rlt_dir/ft/record_info.jsonl
 
 optim_group=attn_kv
 max_epoch=1
-save_dir=$base_dir/ft_gpt/$task/$model_name/${lr}_${SLURM_JOBID}
-# TODO: here too
-# rm -r $save_dir
-# mkdir -p $save_dir
+save_dir=$base_dir/gpt_ft/$task/$model_name/${lr}_${SLURM_JOBID}
 
 mkdir -p $ana_rlt_dir/$ana_setting
 
@@ -108,56 +100,42 @@ bash scripts/ana_train.sh $seed $task $model_path $arch $k $bsz $ngpu $bpe_path 
 
 mv tmp_ana_rlt/${SLURM_JOBID}_ft_record_info.jsonl $ana_rlt_dir/ft/record_info.jsonl
 
-# ==================== analyzing FT setting ============
+# =========== Evaluate FT, ZS, ICL Models ============
+settings="ftzs zs icl"
+for ana_setting in $settings; do
+    case $ana_setting in
+        "ftzs")
+            model_path=$save_dir/checkpoint_last.pt
+            k=0
+            ;;
+        "zs")
+            model_path=$base_dir/gpt_icl/$model_name/model.pt
+            k=0
+            ;;
+        "icl")
+            model_path=$base_dir/gpt_icl/$model_name/model.pt
+            k=$icl_k
+            ;;
+        *)
+            echo "Unknown setting: $ana_setting"
+            exit 1
+            ;;
+    esac
+    echo "Evaluate $ana_setting setting"
+    rm $ana_rlt_dir/$ana_setting/record_info.jsonl
+    mkdir -p $ana_rlt_dir/$ana_setting
 
-k=0
-ana_attn=1
-ana_setting=ftzs
-model_path=$base_dir/ft_gpt/$task/$model_name/${lr}_${SLURM_JOBID}/checkpoint_last.pt
-rm $ana_rlt_dir/ftzs/record_info.jsonl
+    bash scripts/ana_validate.sh $seed $task $model_path $arch $k $bsz $ngpu \
+        $bpe_path $encoder_path \
+        $dict_path $output_path \
+        $ana_attn \
+        $ana_rlt_dir \
+        $ana_setting \
+        $perm_id
+        
+    mv tmp_ana_rlt/${SLURM_JOBID}_${ana_setting}_record_info.jsonl \
+        $ana_rlt_dir/$ana_setting/record_info.jsonl
+done
 
-mkdir -p $ana_rlt_dir/$ana_setting
-
-bash scripts/ana_validate.sh $seed $task $model_path $arch $k $bsz $ngpu $bpe_path $encoder_path $dict_path $output_path \
-    $ana_attn \
-    $ana_rlt_dir \
-    $ana_setting \
-    $perm_id
-
-mv tmp_ana_rlt/${SLURM_JOBID}_ftzs_record_info.jsonl $ana_rlt_dir/ftzs/record_info.jsonl
-
-# ==================== analyzing ZS setting ============
-
-k=0
-ana_attn=1
-ana_setting=zs
-model_path=$base_dir/gpt_icl/$model_name/model.pt
-rm $ana_rlt_dir/zs/record_info.jsonl
-
-mkdir -p $ana_rlt_dir/$ana_setting
-
-bash scripts/ana_validate.sh $seed $task $model_path $arch $k $bsz $ngpu $bpe_path $encoder_path $dict_path $output_path \
-    $ana_attn \
-    $ana_rlt_dir \
-    $ana_setting \
-    $perm_id
-
-mv tmp_ana_rlt/${SLURM_JOBID}_zs_record_info.jsonl $ana_rlt_dir/zs/record_info.jsonl
-
-# ==================== analyzing ICL setting ============
-
-k=$icl_k
-ana_attn=1
-ana_setting=icl
-model_path=$base_dir/gpt_icl/$model_name/model.pt
-rm $ana_rlt_dir/icl/record_info.jsonl
-
-mkdir -p $ana_rlt_dir/$ana_setting
-
-bash scripts/ana_validate.sh $seed $task $model_path $arch $k $bsz $ngpu $bpe_path $encoder_path $dict_path $output_path \
-    $ana_attn \
-    $ana_rlt_dir \
-    $ana_setting \
-    $perm_id
-    
-mv tmp_ana_rlt/${SLURM_JOBID}_icl_record_info.jsonl $ana_rlt_dir/icl/record_info.jsonl
+rm -r $save_dir
+rm -r $output_path
