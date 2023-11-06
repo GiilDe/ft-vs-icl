bc7fad26fc68b6c6ea200c4566b29f0549bf77a5
Guy Dar <dar@mail.tau.ac.il>
2023-11-07 01:16:34 +0200

    refactor scripts folder into run.sh

--- run.sh
+++ run.sh
@@ -21,7 +21,7 @@ while [[ $# -gt 0 ]]; do
             shift 2
             ;;
         --icl_k)
-            icl_k="$2"
+            k="$2"
             shift 2
             ;;
         --output_base_dir)
@@ -33,11 +33,11 @@ while [[ $# -gt 0 ]]; do
             shift 2
             ;;
         --lr)
-            lr=$2
+            lr="$2"
             shift 2
             ;;
         --clip_norm)
-            clip_norm=${2}
+            clip_norm="$2"
             shift 2
             ;;
         --per_layer)
@@ -56,7 +56,7 @@ echo "arch: $arch"
 echo "base_dir: $base_dir"
 echo "seed: $seed"
 echo "task: $task"
-echo "icl_k: $icl_k"
+echo "icl_k: $k"
 echo "output_base_dir: $output_base_dir"
 echo "perm_id: $perm_id"
 echo "lr: $lr"
@@ -64,44 +64,93 @@ echo "clip_norm: $clip_norm"
 echo "per_layer: $per_layer"
 echo "============================================"
 
-bsz=1
-ngpu=1
 bpe_path=$base_dir/gpt_icl/vocab.bpe
 encoder_path=$base_dir/gpt_icl/encoder.json
 dict_path=$base_dir/gpt_icl/$model_name/dict.txt
 output_path=$output_base_dir/${SLURM_JOBID}
 ana_rlt_dir=$base_dir/ana_rlt/$model_name/${task}_${SLURM_JOBID}
+model_path=$base_dir/gpt_icl/$model_name/model.pt
+save_dir=$base_dir/gpt_ft/$task/$model_name/${lr}_${SLURM_JOBID}
+
+bsz=1
+ngpu=1
 ana_attn=1
+
+mkdir -p $output_path
+echo  "output dir: $output_path"
 
 # ============= Train FT Model =============
 echo "Train FT Model"
-k=$icl_k
 ana_setting=ft
-model_path=$base_dir/gpt_icl/$model_name/model.pt
-rm $ana_rlt_dir/ft/record_info.jsonl
-
 optim_group=attn_kv
 max_epoch=1
-save_dir=$base_dir/gpt_ft/$task/$model_name/${lr}_${SLURM_JOBID}
 
+rm $ana_rlt_dir/ft/record_info.jsonl
 mkdir -p $ana_rlt_dir/$ana_setting
 
-bash scripts/ana_train.sh $seed $task $model_path $arch $k $bsz $ngpu $bpe_path $encoder_path $dict_path $output_path \
-    $ana_attn \
-    $ana_rlt_dir \
-    $ana_setting \
-    $lr \
-    $max_epoch \
-    $save_dir \
-    $optim_group \
-    $perm_id \
-    $clip_norm \
-    $per_layer
+python3 validate.py - \
+    --task fs_eval \
+    --tokens-per-sample 2048  \
+    --criterion fs_ft \
+    --arch $arch  \
+    --gpt2-vocab-bpe $bpe_path  \
+    --gpt2-encoder-json $encoder_path \
+    --log-format simple  \
+    --required-batch-size-multiple 1 \
+    --log-interval 1 \
+    --warmup-updates 0 \
+    --optimizer sgd \
+    --lr $lr \
+    --clip-norm $clip_norm \
+    --max-epoch $max_epoch \
+    --curriculum 1000000 \
+    --max-update 1000000 \
+    --fp16 \
+    --eval-data $task \
+    --fp16-init-scale 4 \
+    --fp16-scale-window 256 \
+    --seed $seed \
+    --reset-dataloader \
+    --k $k \
+    --batch-size $bsz \
+    --batch-size-valid $bsz \
+    --ddp-backend=no_c10d \
+    --distributed-no-spawn \
+    --gpt-dict $dict_path \
+    --gpt-model-path $model_path \
+    --ana-attn $ana_attn \
+    --ana-rlt-dir $ana_rlt_dir \
+    --ana-setting $ana_setting \
+    --save-dir $save_dir \
+    --save-interval $max_epoch \
+    --save-interval-updates 1000000 \
+    --validate-interval 1000000 \
+    --disable-validation \
+    --uid $SLURM_JOBID \
+    --optim-group $optim_group \
+    --distributed-world-size $ngpu \
+    --per-layer $per_layer \
+    --permut-index $perm_id |& tee $output_path/train_log_$ana_setting.txt
 
 mv tmp_ana_rlt/${SLURM_JOBID}_ft_record_info.jsonl $ana_rlt_dir/ft/record_info.jsonl
 
 # =========== Evaluate FT, ZS, ICL Models ============
+if [ "$task" = "agnews" ]
+then
+n_classes=4
+fi
+if [ "$task" = "sst5" ]
+then
+n_classes=5
+fi
+if [ "$task" = "cb" ]
+then
+n_classes=3
+fi
+
+bsz_eval=$((n_classes * bsz))
 settings="ftzs zs icl"
+
 for ana_setting in $settings; do
     case $ana_setting in
         "ftzs")
@@ -114,7 +163,7 @@ for ana_setting in $settings; do
             ;;
         "icl")
             model_path=$base_dir/gpt_icl/$model_name/model.pt
-            k=$icl_k
+            k=$k
             ;;
         *)
             echo "Unknown setting: $ana_setting"
@@ -125,13 +174,40 @@ for ana_setting in $settings; do
     rm $ana_rlt_dir/$ana_setting/record_info.jsonl
     mkdir -p $ana_rlt_dir/$ana_setting
 
-    bash scripts/ana_validate.sh $seed $task $model_path $arch $k $bsz $ngpu \
-        $bpe_path $encoder_path \
-        $dict_path $output_path \
-        $ana_attn \
-        $ana_rlt_dir \
-        $ana_setting \
-        $perm_id
+    python3 validate.py "-" \
+    --task fs_eval \
+    --tokens-per-sample 2048  \
+    --criterion fs_eval \
+    --arch $arch  \
+    --gpt2-vocab-bpe $bpe_path  \
+    --gpt2-encoder-json $encoder_path \
+    --log-format simple  \
+    --max-epoch 1 \
+    --required-batch-size-multiple 1 \
+    --log-interval 1 \
+    --warmup-updates 0 \
+    --optimizer sgd \
+    --max-update 0 \
+    --fp16 \
+    --eval-data $task \
+    --fp16-init-scale 4 \
+    --fp16-scale-window 256 \
+    --seed $seed \
+    --reset-dataloader \
+    --no-save \
+    --k $k \
+    --batch-size $bsz_eval \
+    --batch-size-valid $bsz_eval \
+    --ddp-backend=no_c10d \
+    --distributed-no-spawn \
+    --gpt-dict $dict_path \
+    --gpt-model-path $model_path \
+    --ana-attn $ana_attn \
+    --ana-rlt-dir $ana_rlt_dir \
+    --ana-setting $ana_setting \
+    --uid $SLURM_JOBID \
+    --distributed-world-size $ngpu \
+    --permut-index $perm_id |& tee $output_path/train_log_$ana_setting.txt
         
     mv tmp_ana_rlt/${SLURM_JOBID}_${ana_setting}_record_info.jsonl \
         $ana_rlt_dir/$ana_setting/record_info.jsonl
