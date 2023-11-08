ee48853427441f49b989c9be67d7ad1fbf837ac8
Guy Dar <dar@mail.tau.ac.il>
2023-11-08 16:46:28 +0200

    minimal changes for easy experimentation

---
+++ struprompting/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='struprompting',
+    version='0.1',
+    packages=find_packages(),
+    description='StruPrompting',
+    install_requires=[]
+)
